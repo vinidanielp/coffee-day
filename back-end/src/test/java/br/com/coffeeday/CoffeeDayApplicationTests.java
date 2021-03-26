66bebafbf9aeebd0c08d134f59c1d71a722be2b7
Vinicius Daniel <vinicius.pretti2@hotmail.com>
2021-03-26 14:43:34 -0300

    :tada: Add project

---
+++ back-end/src/test/java/br/com/coffeeday/CoffeeDayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coffeeday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
